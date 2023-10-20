edf70720e6c1046f7948d4f4892445297271bd15
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2023-10-20 15:58:50 +0200

    Add functionality to sidebar

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 
+const sidebarButtons = document.querySelectorAll(".overview_button")
+const components = document.querySelectorAll(".component")
 
+sidebarButtons.forEach(button => {
 
+	button.addEventListener("click", ShowComponents)
+})
+
+function ShowComponents(event) {
+
+	const button = event.currentTarget
+	const componentToDisplay = button.dataset.component
+
+	components.forEach(component => {
+		component.classList.remove("component-visible")
+
+		if (component.dataset.id === componentToDisplay) {
+			component.classList.add("component-visible")
+		}
+	})
+
+	sidebarButtons.forEach(button => {
+		button.classList.remove("sidebar-active")
+		
+		if(button.dataset.id === componentToDisplay) {
+			button.classList.add("sidebar-active")
+		}
+	})
+
+}
 
 
 
