c2ef3226d40788006ef2afa0e8fe106bcff9be8e
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2023-10-26 14:02:21 +0200

    Fix sidebarButtons

--- app.js
+++ app.js
@@ -4,32 +4,35 @@ const sidebarButtons = document.querySelectorAll(".overview_button")
 const components = document.querySelectorAll(".component")
 
 sidebarButtons.forEach(button => {
+	function ShowComponents(event) {
+
+		const button = event.currentTarget
+		const componentToDisplay = button.dataset.component
+	
+		sidebarButtons.forEach(button => {
+			button.classList.remove("sidebar-active")
+			
+			if(button.dataset.component === componentToDisplay) {
+				button.classList.add("sidebar-active")
+			}
+		})
+
+		components.forEach(component => {
+			component.classList.remove("component-visible")
+	
+			if (component.dataset.id === componentToDisplay) {
+				component.classList.add("component-visible")
+			}
+		})
+	
+
+	
+	}
 
 	button.addEventListener("click", ShowComponents)
 })
 
-function ShowComponents(event) {
 
-	const button = event.currentTarget
-	const componentToDisplay = button.dataset.component
-
-	components.forEach(component => {
-		component.classList.remove("component-visible")
-
-		if (component.dataset.id === componentToDisplay) {
-			component.classList.add("component-visible")
-		}
-	})
-
-	sidebarButtons.forEach(button => {
-		button.classList.remove("sidebar-active")
-		
-		if(button.dataset.id === componentToDisplay) {
-			button.classList.add("sidebar-active")
-		}
-	})
-
-}
 
 
 
